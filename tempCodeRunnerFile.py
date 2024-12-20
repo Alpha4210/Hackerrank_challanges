6214cd8f2f70bd6c2af7043d9a6a91476d5b526b
Alpha4210 <alpha.gamer.2427@gmail.com>
2024-12-20 20:45:07 +0530

    String formatting completed, (make different rows of decimal, octal, hexadecimal and binary; also set them accordindgly)

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,10 @@
+def print_formatted(n):
+    for i in range(1, n+1):
+        print(i)
+        print(oct(i).replace("0b", ""))
+        print(hex(i).replace("0b", ""))
+        print(bin(i).replace("0b", ""))
 
-# reverse = False
-# counter = 1
-# if (n>5 and n<101) and (m>15 and m<303) and m==3*n:
-#     for i in range(n):
-#         print(char.center(width, "-"))
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
