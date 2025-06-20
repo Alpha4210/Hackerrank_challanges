1d2094fa2cc992658f67798c6bf7575b6c8d24e0
Alpha4210 <alpha.gamer.2427@gmail.com>
2025-06-20 16:38:12 +0530

    Understood the concept of try and except

---
+++ calendar_mod.py
@@ -0,0 +1,8 @@
+import calendar
+E
+x = list(map(int, input().split()))
+
+day = calendar.weekday(x[2], x[0], x[1])
+day_names = list(calendar.day_name)
+
+print(day_names[day].upper())
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
-n = int(input()) #to input the number of customers
-
-for i in range(n):
-    n1,x1 = input(), input()
-    print(n1,x1)
+alendar.weekday(2007, 1, 7))
+# day_nam
